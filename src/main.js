df6f2da6a1aea948455e469979b0f3cd64c68672
Araozu <tbyuyfcjjll@gmail.com>
2020-07-23 21:17:23 -0500

    Eliminado gtag

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import VueGtag from "vue-gtag";
 import router from './router';
 import store from './store';
 import "normalize.css";
@@ -10,9 +9,4 @@ import "./sass/colores.sass";
 const app = createApp(App);
 app.use(router);
 app.use(store);
-/*
-app.use(VueGtag, {
-    config: {id: "UA-113477820-2"}
-}, router);
- */
 app.mount('#app');
