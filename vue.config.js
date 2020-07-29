96e99f329266ee94e23cd6b532735e6d7fa57907
Araozu <tbyuyfcjjll@gmail.com>
2020-07-29 07:35:19 -0500

    Actualizado a Vue 3 RC 5 para abandonar Options API

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
         },
         plugins: [
             new webpack.DefinePlugin({
-                __VUE_OPTIONS_API__: true,
+                __VUE_OPTIONS_API__: false,
                 __VUE_PROD_DEVTOOLS__: true
             })
         ],
