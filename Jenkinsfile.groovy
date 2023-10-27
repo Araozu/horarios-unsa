113505c65ce97336ea2ff728253f48dbe7fbaadc
Araozu <faraoz@unsa.edu.pe>
2023-10-26 21:01:33 -0500

    jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,23 @@
 pipeline {
     agent any
+    environment {
+        PATH = "/var/lib/jenkins/.nvm/versions/node/v20.9.0/bin:/var/lib/jenkins/bin:${env.PATH}"
+    }
     stages {
+        stage('Install') {
+            steps {
+                sh 'pnpm i'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'pnpm build'
+            }
+        }
         stage('Deploy') {
             steps {
                 sh 'rm -rf /var/www/horarios-v2/*'
-                sh 'cp -r ./* /var/www/horarios-v2/'
+                sh 'cp -r ./dist/* /var/www/horarios-v2/'
             }
         }
     }
